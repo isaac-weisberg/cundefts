83107a02bbc122b2d94c387b3f5de38cd185aa17
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-05-25 08:48:28 +0700

    Add general assets to the project

--- src/Project.ts
+++ src/Project.ts
@@ -3,11 +3,13 @@ import { UnversionedSingleatom } from './UnversionedSingleatom'
 import { VersionedSingleAtom } from './VersionedSingleatom'
 import { UnversionedMultiatom } from './UnversionedMultiatom'
 import { VersionedMultiatom } from './VersionedMultiatom'
+import { GeneralAsset } from '.';
 
 export interface Project {
     title: string
     classname: string
     maps: AssetMap[]
+    generals: GeneralAsset
     unsin: UnversionedSingleatom[]
     versin: VersionedSingleAtom[]
     unmul: UnversionedMultiatom[]
